a632748c2a81b17fddf3192010bc9ac6ad6debf1
JonBrant <jonbrant@gmail.com>
2020-07-20 04:51:23 -0400

    String Extensions
    String.To/FromTitleCase

---
+++ Utilities/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+
+namespace Com.PI.Utilities {
+    public static class StringExtensions {
+        public static string ToTitleCase(this string inputString) { return System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase(inputString.ToLower()); }
+
+        public static string FromTitleCase(this string inputString) { return Regex.Replace(inputString, "([a-z](?=[A-Z])|[A-Z](?=[A-Z][a-z]))", "$1 "); }
+    }
+}
