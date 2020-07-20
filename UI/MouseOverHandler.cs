eee0929f23543b170a3c9c1c4dea475f7857333e
JonBrant <jonbrant@gmail.com>
2020-07-20 10:41:06 -0400

    Create MouseOverHandler.cs

---
+++ UI/MouseOverHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+
+namespace Com.PI.UI {
+    public class MouseOverHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+        [Serializable] public class OnMouseEnterUnityEvent : UnityEvent<GameObject> {
+        }
+
+        [Serializable] public class OnMouseExitUnityEvent : UnityEvent<GameObject> {
+        }
+
+        [SerializeField] public OnMouseEnterUnityEvent OnMouseEnter;
+        [SerializeField] public OnMouseExitUnityEvent OnMouseExit;
+
+        public void OnPointerEnter(PointerEventData eventData) {
+            OnMouseEnter?.Invoke(gameObject);
+        }
+
+        public void OnPointerExit(PointerEventData eventData) {
+            OnMouseExit?.Invoke(gameObject);
+        }
+    }
+}
