86b1d0056f9fd13f08bacdf367cb35b561d6e4be
JonBrant <jonbrant@gmail.com>
2020-10-12 03:59:34 -0400

    Singleton

---
+++ StateMachine/PushdownStateMachine/PushdownState.cs
@@ -0,0 +1,22 @@
+﻿#region Using Statements
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#endregion    
+
+public abstract class PushdownState  {
+    public PushdownStateMachine StateMachine {
+        get {
+            return _stateMachine;
+        }
+        set {
+            _stateMachine = value;
+        }
+    }
+    private PushdownStateMachine _stateMachine;
+
+    public virtual string GetStateName() {
+        return "Base Pushdown State";
+    }
+}
---
+++ StateMachine/PushdownStateMachine/States/Unit/ExampleState.cs
@@ -0,0 +1,20 @@
+﻿#region Using Statements
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#endregion
+
+public class ExampleState : PushdownState, IOnStateEnter, IOnStateExit, IOnStateTick {
+    public Vector3 destination;
+
+    public ExampleState() { }
+
+    public override string GetStateName() { return "Example State"; }
+
+    public void OnEnter() { }
+
+    public void OnExit() { }
+
+    public void OnTick() { }
+}
